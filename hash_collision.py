3afc65e9b4d2b2482580726706ef275dbbf5d990
charros96 <88594194+charros96@users.noreply.github.com>
2022-01-23 20:57:48 -0800

    Updated with list

--- hash_collision.py
+++ hash_collision.py
@@ -15,16 +15,33 @@ def hash_collision(k):
     characters = characters + string.ascii_uppercase
     characters = characters + string.digits
     characters = characters + string.punctuation
-
+    
     x = get_string(20, characters)
     y = get_string(20, characters)
+
+    x_bits = get_bits(x,k)
+    y_bits = get_bits(y,k)
     count =0
-    
-    while (get_bits(x,k)!=get_bits(y,k)):
-        y = get_string(20, characters)
+    list_of_strings = [x]
+    new_bits = y_bits
+
+    while ((get_bits(x,k))!=(get_bits(y,k))):
+        new_string = get_string(20,characters)
+        new_bits = get_bits(new_string, k)
+
+        for i in range(len(list_of_strings)):
+            if (new_bits==get_bits(list_of_strings[i],k)):
+                x = list_of_strings[i]
+                y = new_string
+                break
+
+        list_of_strings.append(new_string)        
+
         count= count + 1
         print(count)
     #Collision finding code goes here
+
+
     x = x.encode('utf-8')
     y = y.encode('utf-8')
     
@@ -34,9 +51,10 @@ def get_bits(string, k):
     hex_str = hashlib.sha256(string.encode('utf-8')).hexdigest()
     bit_str = bin(int(hex_str, 16)).zfill(8)
     return(bit_str[-k:])
+
 def get_string(length, characters):
     
     string = ''.join(random.choice(characters) for i in range(length))
     return string
 
-hash_collision(6)
+hash_collision(9)
