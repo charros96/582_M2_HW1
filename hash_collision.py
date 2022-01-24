2cccd71f9e5ab6487a379f066784b8c6f42636f0
charros96 <88594194+charros96@users.noreply.github.com>
2022-01-23 20:29:14 -0800

    Add files via upload

--- hash_collision.py
+++ hash_collision.py
@@ -1,5 +1,7 @@
 import hashlib
 import os
+import random
+import string
 
 def hash_collision(k):
     if not isinstance(k,int):
@@ -9,10 +11,32 @@ def hash_collision(k):
         print( "Specify a positive number of bits" )
         return( b'\x00',b'\x00' )
    
+    characters = string.ascii_lowercase
+    characters = characters + string.ascii_uppercase
+    characters = characters + string.digits
+    characters = characters + string.punctuation
+
+    x = get_string(20, characters)
+    y = get_string(20, characters)
+    count =0
+    
+    while (get_bits(x,k)!=get_bits(y,k)):
+        y = get_string(20, characters)
+        count= count + 1
+        print(count)
     #Collision finding code goes here
-    x = b'\x00'
-    y = b'\x00'
+    x = x.encode('utf-8')
+    y = y.encode('utf-8')
     
     return( x, y )
 
-hash_collision(1)
+def get_bits(string, k):
+    hex_str = hashlib.sha256(string.encode('utf-8')).hexdigest()
+    bit_str = bin(int(hex_str, 16)).zfill(8)
+    return(bit_str[-k:])
+def get_string(length, characters):
+    
+    string = ''.join(random.choice(characters) for i in range(length))
+    return string
+
+hash_collision(6)
